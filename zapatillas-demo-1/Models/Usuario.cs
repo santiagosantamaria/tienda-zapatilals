61ad2b6f17ddf1b8b06d4b39de019705a2f04df0
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2021-10-06 23:00:12 -0300

    clase usuario fix

--- zapatillas-demo-1/Models/Usuario.cs
+++ zapatillas-demo-1/Models/Usuario.cs
@@ -1,16 +1,20 @@
 ﻿using System;
-
-public class Class1
+namespace zapatillas_demo_1.Models
 {
-	public Class1
+
+	public class Usuario
 	{
 		public int id_usuario { get; set; }
 		public int id_rol { get; set; }
 		public String email { get; set; }
 		public String password { get; set; }
-
-
+	
+	
+		public Usuario()
+		{
+		}
 
 
 	}
+
 }
