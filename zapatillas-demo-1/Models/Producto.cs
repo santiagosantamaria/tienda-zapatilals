c967945166662a5ce3acb79b6602378cb25ad411
Santiago Santamaria <santiago.santamaria@gmail.com>
2021-10-06 22:07:36 -0300

    zapatillas demo base

---
+++ zapatillas-demo-1/Models/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace zapatillas_demo_1.Models
+{
+    public class Producto
+    {
+     
+        public int id_producto { get; set; }
+        public String cod_producto { get; set; }
+        public String foto { get; set; }
+        public int cantidad { get; set; }
+        public float talle { get; set; }
+        public String descripcion { get; set; }
+        public float precio { get; set; }
+        public float en_stock { get; set; }
+
+
+        public Producto()
+        {
+        }
+    }
+}
