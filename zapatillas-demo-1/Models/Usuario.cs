b11962ef4779eaaf47a08f1a2f179443e93ae343
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2021-10-06 22:55:15 -0300

    clase usuario
    add clase usuario y sus atributos

---
+++ zapatillas-demo-1/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1
+	{
+		public int id_usuario { get; set; }
+		public int id_rol { get; set; }
+		public String email { get; set; }
+		public String password { get; set; }
+
+
+
+
+	}
+}
