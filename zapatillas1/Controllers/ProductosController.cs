ee57bb2e099113329b88b46baf771d4a92a0a625
Santiago Santamaria <santiago.santamaria@gmail.com>
2021-10-20 21:26:00 -0300

    productos en pagina productos
    productos en pagina productos

--- zapatillas1/Controllers/ProductosController.cs
+++ zapatillas1/Controllers/ProductosController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using zapatillas1.zapatillas1.Models;
+using zapatillas1.zapatillas1.Data;
+using System.Linq;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,9 +13,15 @@ namespace zapatillas1.Controllers
 {
     public class ProductosController : Controller
     {
-        
+        private readonly EshopDbContext db;
+        public ProductosController(EshopDbContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.productos = db.Productos.ToList();
             return View();
         }
     }
