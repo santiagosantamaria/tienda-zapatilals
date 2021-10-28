4f4d022aeffffd759cdccc8603f607daccef7250
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2021-10-27 21:25:23 -0300

    Create Rol.cs

---
+++ zapatillas1/Models/Rol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace zapatillas1.zapatillas1.Models
+{
+    public class Rol
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Id_Usuario { get; set; }
+        public int Permisos { get; set; }
+
+
+    }
+}
