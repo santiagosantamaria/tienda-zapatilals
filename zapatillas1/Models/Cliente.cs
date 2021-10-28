0b24c22ae6e4c5f168b5604617d440c49cc7cfee
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2021-10-27 21:15:14 -0300

    Creacion model cliente

---
+++ zapatillas1/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace zapatillas1.zapatillas1.Models
+{
+    public class Cliente
+    {
+
+        ArrayList CarritoProductos = new ArrayList();
+
+
+    }
+}
