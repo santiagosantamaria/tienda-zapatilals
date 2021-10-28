e09ce3cc418245bd02cd175d04f6446a8a47bbd9
Santiago Santamaria <santiago.santamaria@gmail.com>
2021-10-27 21:20:31 -0300

    Usuario creado
    Usuario creado

---
+++ zapatillas1/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace zapatillas1.zapatillas1.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id_usuario { get; set; }
+        public int Id_rol { get; set; }
+        public String email { get; set; }
+        public String password { get; set; }
+                            
+    }
+}
