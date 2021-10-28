f20e81d98239348b1c43cb9ff6c8c7cab7eb46e5
Federico Medel <88804900+FedeMedel@users.noreply.github.com>
2021-10-27 21:19:57 -0300

    Create Stock.cs

---
+++ zapatillas1/Models/Stock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace zapatillas1.zapatillas1.Models
+{
+    public class Stock
+    {
+        [Key]
+        public int id_producto { get; set; }
+        public bool hay_stock { get; set; }
+
+
+
+    }
+}
