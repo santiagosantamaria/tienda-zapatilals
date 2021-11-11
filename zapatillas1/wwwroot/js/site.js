de5ae3fd9ad75d156baf01c7922170d1f22daf24
Santiago Santamaria <santiago.santamaria@gmail.com>
2021-11-11 10:53:14 -0300

    chequear que haya un talle ingresado via js
    chequear que haya un talle ingresado via js

--- zapatillas1/wwwroot/js/site.js
+++ zapatillas1/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById('link-agregar').onclick = function () {
+    
+    talle = document.getElementById("talle-zapa").value;
+
+    if(talle == "ingrese-talle") {
+        alert("Ingrese un talle");
+    } else {
+        var codProducto = document.getElementById("cod-producto").value;
+        window.location.href = "/Carrito/Add/" + codProducto + "-" + talle;
+    }
+        
+};
