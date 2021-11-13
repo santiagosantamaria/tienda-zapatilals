5c5e9ba5da47d060b89dc48f1d0eca285de01d10
CurelloAlejoMF <curello.alejo@gmail.com>
2021-11-13 17:13:42 -0300

    Authorize en productos

--- zapatillas1/Controllers/ProductosController.cs
+++ zapatillas1/Controllers/ProductosController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -62,6 +63,7 @@ namespace zapatillas1.Controllers
 
 
         // GET: Productos/Stock
+        [Authorize]
         public async Task<IActionResult> Stock()
         {
 
@@ -101,6 +103,7 @@ namespace zapatillas1.Controllers
 
 
         // GET: Productos/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -111,6 +114,7 @@ namespace zapatillas1.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Create([Bind("Id,Cod_producto,Foto,Cantidad,Talle,Descripcion,Precio,En_stock")] Producto producto)
         {
             if (ModelState.IsValid)
@@ -123,6 +127,7 @@ namespace zapatillas1.Controllers
         }
 
         // GET: Productos/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -142,6 +147,7 @@ namespace zapatillas1.Controllers
         // POST: Productos/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Cod_producto,Foto,Cantidad,Talle,Descripcion,Precio,En_stock")] Producto producto)
         {
             if (id != producto.Id)
@@ -174,6 +180,7 @@ namespace zapatillas1.Controllers
         }
 
         // GET: Productos/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -194,6 +201,7 @@ namespace zapatillas1.Controllers
         // POST: Productos/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var producto = await _context.Productos.FindAsync(id);
