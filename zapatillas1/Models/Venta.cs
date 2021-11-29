7420dcbaec01368abf8e7e8aa74172b9760cb89a
CurelloAlejoMF <curello.alejo@gmail.com>
2021-11-29 20:27:56 -0300

    Models ventas y ventasxProducto

---
+++ zapatillas1/Models/Venta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zapatillas1.Models
+{
+    public class Venta
+    {
+        [Key]
+        public int Id_Venta { get; set; }
+
+        public float MontoTotal { get; set; }
+
+        public String Fecha { get; set; }
+
+    }
+}
---
+++ zapatillas1/Models/VentaXProducto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zapatillas1.Models
+{
+    public class VentaXProducto
+    {
+        [Key]
+        public int Id_Venta { get; set; }
+        [Key]
+        public int Id_Producto { get; set; }
+    }
+}
