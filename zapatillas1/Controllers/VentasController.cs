e9a77cc5a755ea4a917c48410b95aba3b4088db1
CurelloAlejoMF <curello.alejo@gmail.com>
2021-11-30 20:53:12 -0300

    Negrada de Code
    Tenemos un historial de ventas, pero Ulman de reprueba 15 veces

---
+++ zapatillas1/Controllers/VentasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using zapatillas1.zapatillas1.Data;
+using zapatillas1.zapatillas1.Models;
+
+namespace zapatillas1.Controllers
+{
+    public class VentasController : Controller
+    {
+        private readonly EshopDbContext _context;
+
+        public VentasController(EshopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ventas
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Productos = _context.Productos.ToArray();
+            ViewBag.VentasXProductos = _context.VentaXProductos.ToArray();
+            return View(await _context.Ventas.ToListAsync());
+        }
+    }
+}
