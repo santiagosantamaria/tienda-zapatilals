0b35a07dee3acc5495ed6240eba674826c3185f6
Santiago Santamaria <santiago.santamaria@gmail.com>
2021-12-01 13:49:05 -0300

    historial para usuarios logueados - nav - detalles
    historial para usuarios logueados - nav - detalles

--- zapatillas1/Controllers/VentasController.cs
+++ zapatillas1/Controllers/VentasController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using zapatillas1.zapatillas1.Data;
 using zapatillas1.zapatillas1.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace zapatillas1.Controllers
 {
@@ -20,6 +21,7 @@ namespace zapatillas1.Controllers
         }
 
         // GET: Ventas
+        [Authorize]
         public async Task<IActionResult> Index()
         {
             ViewBag.Productos = _context.Productos.ToArray();
