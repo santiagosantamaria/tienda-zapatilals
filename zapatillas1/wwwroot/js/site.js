fc64ba6fdb71cfab31832200127589a04711b853
Santiago Santamaria <santiago.santamaria@gmail.com>
2021-12-02 14:37:49 -0300

    cambios en estilos - detalle js
    cambios en estilos - detalle js

--- zapatillas1/wwwroot/js/site.js
+++ zapatillas1/wwwroot/js/site.js
@@ -1,36 +1,35 @@
-﻿
-$('#talle-zapa').on('change', function() {
-    
+﻿$('#talle-zapa').on('change', function() {
+
     let talle = document.getElementById("talle-zapa").value;
-    let idProducto  = document.getElementById('id-producto').value;
+    let idProducto = document.getElementById('id-producto').value;
     let codProducto = document.getElementById("cod-producto").value;
-    
-    
+
+
     $.ajax({
         type: "GET",
         url: '/Productos/Cantidad',
         contentType: "application/json; charset=utf-8",
-        data: { 
-            id: idProducto ,
-            talle: talle ,
-            codp: codProducto ,
-        
+        data: {
+            id: idProducto,
+            talle: talle,
+            codp: codProducto,
+
         },
 
         dataType: "json",
-        success: function(resp){ 
+        success: function(resp) {
             console.log(resp);
-            var idProducto      = resp[0];
-            var codProducto     = resp[1];
-            var cantidadStock   = resp[2];
-            
+            var idProducto = resp[0];
+            var codProducto = resp[1];
+            var cantidadStock = resp[2];
+
             document.getElementById('id-producto').value = idProducto;
             // document.getElementById('cantidad-total-stock-item').innerHTML = cantidadStock;
-            
+
             let stringSelect1 = '<select name="cant-items" id="cant-items">';
             let stringSelect2 = '';
             let stringSelect3 = '</select>';
-            
+
 
             for (i = 1; i <= cantidadStock; i++) {
                 stringSelect2 += '<option value=' + i + '>' + i + '</option>'
@@ -43,17 +42,17 @@ $('#talle-zapa').on('change', function() {
 
         error: console.log("error")
     });
-    
-    console.log( 'id desde html: ' +  idProducto );
-    
-    document.getElementById('link-agregar').onclick = function () {
-        
-        let idProducto  = document.getElementById('id-producto').value;
-        let cantidadItemsCompra  = document.getElementById('cant-items').value;
+
+    console.log('id desde html: ' + idProducto);
+
+    document.getElementById('link-agregar').onclick = function() {
+
+        let idProducto = document.getElementById('id-producto').value;
+        let cantidadItemsCompra = document.getElementById('cant-items').value;
 
         // select para la cantidad = cant-zapadiv
         // id-producto .value tiene el id
-        
+
         $.ajax({
             type: "GET",
             url: '/Carrito/Add',
@@ -63,7 +62,7 @@ $('#talle-zapa').on('change', function() {
                 cantidad: cantidadItemsCompra,
             },
             dataType: "json",
-            success: function (resp) {
+            success: function(resp) {
                 console.log(resp);
                 window.location.href = "/Carrito/Ver/";
             },
@@ -75,14 +74,19 @@ $('#talle-zapa').on('change', function() {
     };
 
 
-  });
+});
 
 
-    // buscar historial modelo talle
+// buscar historial modelo talle
 
-    document.getElementById('buscar-historial-modelo').onclick = function () {
-        
-        let idProducto  = document.getElementById('select-producto-id').value;
-        window.location.href = "/Ventas/Zapatilla?id="+idProducto;
-        
-    };
+document.getElementById('buscar-historial-modelo').onclick = function() {
+
+    let idProducto = document.getElementById('select-producto-id').value;
+    console.log(idProducto);
+    if (idProducto != "-1") {
+        window.location.href = "/Ventas/Zapatilla?id=" + idProducto;
+    } else {
+        alert("Elegi un modelo");
+    }
+
+};
